e5123f75051c16f107f47389c7665139b6f10677
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-08 22:51:39 +0530

    Added: Enter a valid first name

--- src/main/java/com/validation/UserReg.java
+++ src/main/java/com/validation/UserReg.java
@@ -1,8 +1,19 @@
+
 package com.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserReg {
 	
-
+	public static boolean isFirstName(String firstname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern patt = Pattern.compile(regex);
+		if (firstname == null) {
+			return false;
+		}
+		Matcher match = patt.matcher(firstname);
+		return match.matches();
+	}
 
 }
--- src/test/java/com/validation/UserRegistrationTest.java
+++ src/test/java/com/validation/UserRegistrationTest.java
@@ -1,7 +1,45 @@
 package com.validation;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
 
 
 public class UserRegistrationTest {
+	UserReg userReg;
+
+	@Before
+	public void setUp() {
+		userReg = new UserReg();
+
+	}
+	@Test
+	public void givenName_WhenFirstNameCaptial_ShouldReturn() {
+
+		boolean actual = userReg.isFirstName("Shubha");
+		Assert.assertTrue(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstName_FirstCharacter_IsInLowerCase() {
+
+		boolean actual = userReg.isFirstName("shubha");
+		Assert.assertFalse(actual);
+	}
+
+	@Test
+	public void givenName_WhenFirstName_SpecialCharacter_IsThere_OrNot() {
+
+		boolean actual = userReg.isFirstName("Shubha@");
+		Assert.assertFalse(actual);
+	}
+
+	
+	@Test
+	public void givenName_WhenFirstName_HaveNumbers() {
+
+		boolean actual = userReg.isFirstName("Shubha123");
+		Assert.assertFalse(actual);
+	}
 
 }
