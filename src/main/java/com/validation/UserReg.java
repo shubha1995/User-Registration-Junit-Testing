065ed78c119a9d8239927d051c9327ae5f7900f5
shubhasankar <ssbhaumikdmr@gmail.com>
2021-07-14 22:47:35 +0530

    Added: Write junit test for validate the entries

--- src/main/java/com/validation/UserReg.java
+++ src/main/java/com/validation/UserReg.java
@@ -1,12 +1,12 @@
-	
+
 package com.validation;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserReg {
-	//Check First Name
-	
+	// Check First Name
+
 	public static boolean isFirstName(String firstname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -16,9 +16,9 @@ public class UserReg {
 		Matcher match = patt.matcher(firstname);
 		return match.matches();
 	}
-	
-	//Check Last Name
-	
+
+	// Check Last Name
+
 	public static boolean isLastName(String lastname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -28,10 +28,9 @@ public class UserReg {
 		Matcher match = patt.matcher(lastname);
 		return match.matches();
 	}
-	
-	
-	//Check Email
-	
+
+	// Check Email
+
 	public static boolean isEmail(String email) {
 		String regex = "^[a-zA-Z]+([.][A-Za-z]+)*@[a-zA-Z]+[.]+[a-zA-Z]{2,3}+([.][A-Za-z]{2}+)*$";
 		Pattern patt = Pattern.compile(regex);
@@ -41,9 +40,9 @@ public class UserReg {
 		Matcher match = patt.matcher(email);
 		return match.matches();
 	}
-	
-	//Check Mobile Format
-	
+
+	// Check Mobile Format
+
 	public static boolean isMobileNo(String mobile) {
 		String regex = "^[0-9]{2}\\s{0,1}[0-9]{10}$";
 		Pattern patt = Pattern.compile(regex);
@@ -53,9 +52,9 @@ public class UserReg {
 		Matcher match = patt.matcher(mobile);
 		return match.matches();
 	}
-	
-	//Check Password Rule 1
-	
+
+	// Check Password Rule 1
+
 	public static boolean isPassword(String password) {
 		String regex = "^[a-zA-Z]{8,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -65,9 +64,9 @@ public class UserReg {
 		Matcher match = patt.matcher(password);
 		return match.matches();
 	}
-	
-	//Check Password Rule 2
-	
+
+	// Check Password Rule 2
+
 	public static boolean isPassword1(String password1) {
 		String regex = "^[A-Z]{1}+[a-zA-Z]{7,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -77,9 +76,9 @@ public class UserReg {
 		Matcher match = patt.matcher(password1);
 		return match.matches();
 	}
-	
-	//Check Password Rule 3
-	
+
+	// Check Password Rule 3
+
 	public static boolean isPassword2(String password2) {
 		String regex = "^[A-Z]{1}+[a-zA-Z].+[0-9].+{8,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -89,9 +88,9 @@ public class UserReg {
 		Matcher match = patt.matcher(password2);
 		return match.matches();
 	}
-	
-	//Check Password Rule 4
-	
+
+	// Check Password Rule 4
+
 	public static boolean isPassword3(String password3) {
 		String regex = "^[A-Z]{1}+[a-zA-Z].*[@#$%^&*._+=].*[0-9].*{8,}$";
 		Pattern patt = Pattern.compile(regex);
@@ -101,9 +100,5 @@ public class UserReg {
 		Matcher match = patt.matcher(password3);
 		return match.matches();
 	}
-	
-
-
-
 
 }
